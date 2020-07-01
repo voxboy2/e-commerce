6a7ab91edbfed4287fed68db30bafa554ec98a91
voxboy2 <ebieroma323@gmail.com>
2020-07-01 21:01:11 +0100

    f

--- server/index.js
+++ server/index.js
@@ -50,10 +50,10 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   // index.html for all page routes    html or routing and naviagtion
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build'))
-})
-
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(isaac, "../client", "build", "index.html"));
+  });
+  
 }
 
 const port = process.env.PORT || 5000
